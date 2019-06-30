7f044dcd2ea2a01d4f1069f145a9d42e994eb739
Tim Blanchard <tim@Tims-MacBook-Pro.local>
2019-06-30 16:40:39 +0100

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,26 +6,34 @@ const name = "Tim's";
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { tasks: [] }
+    this.state = { tasks: [], filteredTasks: [], searchTerm: "" }
+    this.handleSearchTerm = this.handleSearchTerm.bind(this)
   }
   componentDidMount() {
     fetch('http://localhost:8000/tasks')
       .then(response => response.json())
       .then(data => this.setState({ tasks: data }))
   }
+  handleSearchTerm(e) {
+    console.log(e.target.value)
+    // let filteredData = [...this.state.tasks];
+    let searchTerm = e.target.value.toLowerCase()
+    this.setState(prevState => {
+      let filteredData = prevState.tasks.filter(x => x.title.toLowerCase().includes(searchTerm))
+      return { filteredTasks: filteredData, searchTerm: searchTerm }
+    });
+    console.log('filtered state', this.state)
+  }
   render() {
-    const { tasks } = this.state
-    console.log('this is the state', tasks)
+    const { tasks } = this.state;
     return (
       <div className="App" >
-        <h1> {name} Todo List git test</h1>
+        <h1> {name} Todo List</h1>
         <div className="outer-container">
-          hello
-          {console.log('this is state from render method', this.state.tasks)}
+          <input type="text" className="input" placeholder="Search..." onChange={this.handleSearchTerm} />
           {tasks.map((task, key) => {
             return <Task title={task.title} dueDate={task.dueDate} key={key} />
           })}
-
         </div>
       </div >
     );
