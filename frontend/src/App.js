ae64ff3fbc55dd4b98b8d8108335b1a9ab143c90
Tim Blanchard <tim@Tims-MacBook-Pro.local>
2019-07-02 20:55:41 +0100

    can now add new task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,34 +6,83 @@ const name = "Tim's";
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { tasks: [], filteredTasks: [], searchTerm: "" }
-    this.handleSearchTerm = this.handleSearchTerm.bind(this)
+    this.state = {
+      tasks: [],
+      filteredTasks: [],
+      searchTerm: "",
+      newNoteTitle: "",
+      newNoteDate: ""
+    }
+    this.handleSearchTerm = this.handleSearchTerm.bind(this);
+    this.handleNewTaskTitle = this.handleNewTaskTitle.bind(this);
+    this.handleNewTaskDate = this.handleNewTaskDate.bind(this);
+    this.handleNewTaskAdd = this.handleNewTaskAdd.bind(this);
+
+
   }
   componentDidMount() {
     fetch('http://localhost:8000/tasks')
       .then(response => response.json())
-      .then(data => this.setState({ tasks: data }))
+      .then(data => this.setState({ tasks: data, filteredTasks: data }))
   }
   handleSearchTerm(e) {
-    console.log(e.target.value)
     // let filteredData = [...this.state.tasks];
     let searchTerm = e.target.value.toLowerCase()
     this.setState(prevState => {
-      let filteredData = prevState.tasks.filter(x => x.title.toLowerCase().includes(searchTerm))
-      return { filteredTasks: filteredData, searchTerm: searchTerm }
+      if (searchTerm) {
+        let filteredData = prevState.tasks.filter(x => x.title.toLowerCase().includes(searchTerm))
+        return { filteredTasks: filteredData, searchTerm: searchTerm }
+      } else {
+        return { filteredTasks: prevState.tasks, searchTerm: searchTerm }
+      }
     });
-    console.log('filtered state', this.state)
   }
+
+  handleNewTaskTitle(e) {
+    let newNoteTitle = e.target.value
+    this.setState(prevState => {
+      if (newNoteTitle) {
+        return { newNoteTitle: newNoteTitle }
+      }
+    });
+  }
+
+  handleNewTaskDate(e) {
+    let newNoteDate = e.target.value
+    this.setState(prevState => {
+      if (newNoteDate) {
+        return { newNoteDate: newNoteDate }
+      }
+    });
+  }
+
+  handleNewTaskAdd() {
+    this.setState(prevState => {
+      if (prevState.newNoteTitle && prevState.newNoteDate) {
+        return {
+          filteredTasks: [...prevState.filteredTasks, { title: prevState.newNoteTitle, dueDate: prevState.newNoteDate }]
+        }
+      }
+    })
+  }
+
+
+  ///THREE THINGS WHEN ADDING NEW NOTE - clear fields - reset state 
+
+
+
   render() {
-    const { tasks } = this.state;
+    const { filteredTasks } = this.state;
     return (
       <div className="App" >
         <h1> {name} Todo List</h1>
         <div className="outer-container">
           <input type="text" className="input" placeholder="Search..." onChange={this.handleSearchTerm} />
-          {tasks.map((task, key) => {
+          {filteredTasks.map((task, key) => {
             return <Task title={task.title} dueDate={task.dueDate} key={key} />
           })}
+          <input type="text" className="input" placeholder="Note Title..." onChange={this.handleNewTaskTitle} />
+          <input type="text" className="input" placeholder="Due Date..." onChange={this.handleNewTaskDate} /> <span className="new-task-button" onClick={this.handleNewTaskAdd}>add new task</span>
         </div>
       </div >
     );
